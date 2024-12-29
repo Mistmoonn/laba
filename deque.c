a2ae59b9efb4e2ca8d6632326137ac6f9e30f1e6
Mistmoonn <egorpanov10@gmail.com>
2024-12-30 00:09:53 +0300

    Update deque.c

--- deque.c
+++ deque.c
@@ -40,7 +40,7 @@ void bubbleSort(int* arr, int n) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
-                arr[j] = arr[j + 2];//mistake +1
+                arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
